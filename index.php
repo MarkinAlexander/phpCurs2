a0403a1cadea30da0395784e2d811801591562bc
Александр Маркин <sashamarkin@yandex.ru>
2019-04-27 00:48:18 +0300

    ДЗ к уроку 3

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'models/config.php';
+include_once 'models/photo.php';
+// подгружаем и активируем авто-загрузчик Twig-а
+require_once 'Twig/Autoloader.php';
+Twig_Autoloader::register();
+try {
+  // указывае где хранятся шаблоны
+  $loader = new Twig_Loader_Filesystem('templates');
+  
+  // инициализируем Twig
+  $twig = new Twig_Environment($loader);
+  
+  // подгружаем шаблон
+  $template = $twig->loadTemplate('index.tmpl');
+  
+  // передаём в шаблон переменные и значения
+  // выводим сформированное содержание
+  
+  $content = $template->render(array(
+    'title' => 'Каталог изображений',
+    'form' => $form,
+    'message' => $message,
+  ));
+  echo $content;
+  
+} catch (Exception $e) {
+  die ('ERROR: ' . $e->getMessage());
+}
+?>
+
+
+
+
+
+
